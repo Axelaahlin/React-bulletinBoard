87c54a3c850d62a99770f0639c8f5c4458c16d30
axelaahlin <axel.ahlinandersson@yh.nackademin.se>
2022-09-02 10:11:29 +0200

    changes

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,17 +4,25 @@ import Likedpost from "./Likepost"
 
 let Question = function (props) {
 const [showAnswer, setShowAnswer] = useState(false)
+const [liked, setLiked] = useState(false)
 
 let show = function () {
   setShowAnswer(!showAnswer)  
+}
 
+let changeLiked = () => {
+  setLiked(!liked)
 }
   return ( 
     <div className="question"> 
       <h2>{props.FAQ.question}</h2>
       <h3 className={!showAnswer && "hidden"}>{props.FAQ.answer}</h3>
       <button onClick={() => {show()}}>{showAnswer ? "Dölj svar" : "Visa svar"}</button>
-      <Likedpost show={showAnswer}/>
+      
+      <div className={!showAnswer && "hidden"}>
+      <button onClick={() => {changeLiked()}} className={liked && "hidden"} >Gilla svaret</button>
+      <Likedpost show={showAnswer} liked={liked}/>
+      </div>
     </div>
   )
 }
